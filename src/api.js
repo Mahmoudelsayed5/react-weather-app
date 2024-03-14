012b39debbd46ac45b2bd1e60c013004be1a3a3e
mahmoudelsayed5 <mahmoudelsayed2@aiesec.net>
2024-03-14 06:13:37 +0200

    Weather application using the OpenWeatherMap API and GeoDB API with cities autocomplete.

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5bb4d69d07msh8dab6b6d43613d1p1df726jsn6a8574096cd7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "18948c18f8c893dd5ba8bb4467f48266";
